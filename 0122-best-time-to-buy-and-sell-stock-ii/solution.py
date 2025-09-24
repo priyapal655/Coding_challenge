dd59aac900d0450eb326bde15bd008a18168f3fc
Priya pal <priyapal655@hotmail.com>
2025-09-24 19:57:16 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 13.6 MB (46.78%)

---
+++ 0122-best-time-to-buy-and-sell-stock-ii/solution.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        if not prices or len(prices) < 2:
+            return 0
+    
+        profit = 0
+        for i in range(1, len(prices)):
+            if prices[i] > prices[i-1]:
+                profit += prices[i] - prices[i-1]
+        
+        return profit
+
+        
